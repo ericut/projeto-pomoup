6a96ecc37c9617d07c737a75314830f9895c3a44
Eric Li <ericut@gmail.com>
2021-02-25 11:18:14 -0300

    Level up, Notifications

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext)
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/ericut.png" alt="Eric Frank Li" />
@@ -8,7 +11,7 @@ export function Profile() {
         <strong>Eric Frank Li</strong>
         <p>
           <img src="icons/level.svg" alt="Level"></img>
-          Level 1
+          Level { level }
         </p>
       </div>
     </div>
